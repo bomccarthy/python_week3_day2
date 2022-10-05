51d0c263f7ba91f3849cd0908df60a9ea3efa4da
Bo McCarthy <sysrock@gmail.com>
2022-10-05 16:58:03 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+# Input: ([1, 6, 9, -3, 4, -78, 0], -3, 4)				
+# Output: True
+# Input: ([3,1,0,19], 19, 0)	
+# Output: True
+
+
+
+# def isInArray(arr, a, b):
+#     return arr.index(a) + 1 == arr.index(b) or arr.index(a) - 1 == arr.index(b)
+# 
+# print(isInArray([1, 6, 9, -3, 4, -78, 0], -3, 4))
+
